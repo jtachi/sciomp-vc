b18197cb87c0c7e7ed464d3a645705cb88f039c7
Ashley Chi <jtachi@yahoo.com>
2015-10-06 10:06:54 -0400

    this is a reviert
    Revert "I have made a changes in the files0"
    
    This reverts commit ab728af514eea268dac2562c3738949562e6b6e1.

--- detect.py
+++ detect.py
@@ -6,7 +6,11 @@ def detect_problems(filename):
     data.columns = ['chrom','chromStart','chromEnd','name','score','strand','level','signif','score2']
     if data['score2'].min() < 1 and data['score'].min() > 0:
         print 'Suspicious data!'
+<<<<<<< HEAD
     elif data.loc[data['chrom'] == 'chrM']['score'].mean() > 50:
+=======
+    elif data.loc[data['chrom'] == 'chrM']['score'].mean() > 200:
+>>>>>>> parent of ab728af... I have made a changes in the files0
         print 'High scores on chrM!'
     else:
         print 'Seems OK!'
